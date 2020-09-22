f22f11800e7d67663b9cd855f2eb3a8ef7fa4a91
Siyami Avci <siyamiavci@gmail.com>
2020-09-22 15:45:07 -0700

    fisnish CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:10PM" comment="c1" imgSrc={faker.image.avatar()} />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="c2" imgSrc={faker.image.avatar()} />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:55PM" comment="c3" imgSrc={faker.image.avatar()} />
     </div>
   );
 }
