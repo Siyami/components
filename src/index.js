09d65baaf97b1408d22eb547b316cd56a4b15096
Siyami Avci <siyamiavci@gmail.com>
2020-09-23 00:33:26 -0700

    finish ApprovalCard  component

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail author="Sam" timeAgo="Today at 4:10PM" comment="c1" imgSrc={faker.image.avatar()} />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="c2" imgSrc={faker.image.avatar()} />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:55PM" comment="c3" imgSrc={faker.image.avatar()} />
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:10PM" comment="c1" imgSrc={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="c2" imgSrc={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterday at 5:55PM" comment="c3" imgSrc={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 }
